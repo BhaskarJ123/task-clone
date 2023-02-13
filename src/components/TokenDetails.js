c2f11f92a8ca91d101799df4d0b02cf6a4b87d72
Bhaskar Jha <bhaskar.22.1@mountblue.tech>
2023-02-14 02:57:37 +0530

    Updated the UI.

--- src/components/TokenDetails.js
+++ src/components/TokenDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams,Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import '../TokenDetails.css';
 
@@ -11,15 +11,18 @@ const TokenDetails = () => {
     let selectedToken = tokens.find((token) => {
         return token.id === parseInt(params.id);
     });
-
+    console.log(selectedToken);
     console.log(selectedToken);
     return (
         <div className="tokenDetailsContainer">
-            <h1>Token Details</h1>
-            <h4>Token No: {selectedToken.token_number}</h4>
-            <h4>Status: {selectedToken.status}</h4>
-            <h4>Domain: {selectedToken.domain_name}</h4>
-            <button type="button" class="btn btn-danger">Suspend</button>    
+            <h1>TOKEN DETAILS</h1>
+            <div className="tokenDetails">
+                <h4><span className="detailsToken">TOKEN NO: </span><span className="mutedText">{selectedToken.token_number}</span></h4>
+                <h4><span className="detailsStatus">STATUS: </span><span className="mutedText">{selectedToken.status}</span></h4>
+                <h4><span className="detailsDomain">DOMAIN: </span><span className="mutedText">{selectedToken.domain_name}</span></h4>
+                <button type="button" class="btn btn-danger">Suspend Token</button>
+                <Link to={`/card/${selectedToken.card_id}`}><button type="button" className="btn backButton">Back</button></Link>
+            </div>
         </div>
     )
 }
